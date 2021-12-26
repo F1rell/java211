f33f20b517cb16d1f0cb0900d7583f083e9da359
F1rell <ratmir777mustafin@mail.ru>
2021-12-26 18:50:54 +0700

    Бонусы за каждые 20р

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 213; // цена билета
+        int bonusMile = ticketPrice / 20; // бонусные мили за каждые 20р
+     System.out.println("Ваш бонус " + bonusMile + " миль");
+    }
+}
